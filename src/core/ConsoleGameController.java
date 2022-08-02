b9d1be3e220491c1dcfe9bd38342a1060ece3676
qingzhixing <1418390991@qq.com>
2022-08-02 19:16:25 +0800

    ConsoleGameController

--- src/core/ConsoleGameController.java
+++ src/core/ConsoleGameController.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.Scanner;
+
 public class ConsoleGameController extends AbstractGameController{
     public ConsoleGameController(int boardSize,AbstractPlayer blackPlayer,AbstractPlayer whitePlayer){
         super(boardSize,blackPlayer,whitePlayer);
@@ -13,16 +15,25 @@ public class ConsoleGameController extends AbstractGameController{
 
     @Override
     protected void DisplayBoard() {
+        System.out.println("----display board----");
         chessBoard.DebugPrintOnConsole();
     }
 
     @Override
     protected void DisplayWinner(ChessPiece winner) {
-
+        System.out.println("----display winner----");
+        System.out.println("Winner is " + winner);
     }
 
     @Override
-    protected boolean IsAbleToRestart() {
-        return false;
+    protected boolean IsAbleToRestart(){
+        boolean isAbleToRestart=false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to restart?(y/n)");
+        if(scanner.hasNext()){
+            isAbleToRestart= scanner.next().equals("y");
+        }
+        scanner.close();
+        return isAbleToRestart;
     }
 }
