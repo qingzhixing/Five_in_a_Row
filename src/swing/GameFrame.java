7a2e94ff0b41114648fa76955d7b76e07154a5bc
qingzhixing <1418390991@qq.com>
2022-08-04 19:26:35 +0800

    why I cannot read icon file even it's checked by program?

--- src/swing/GameFrame.java
+++ src/swing/GameFrame.java
@@ -1,18 +1,31 @@
 package swing;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class GameFrame extends JFrame {
 
     /*
     根据棋盘大小，格子变长，内间距计算窗口大小
      */
-    public GameFrame(int boardSize, int gridSideLength, int padding) {
+    public GameFrame(int boardSize, int gridSideLength, int padding) throws IOException {
         int length = gridSideLength*(boardSize+1)+padding*2;
         setSize(length, length);
         setResizable(false);
         setTitle("Five in a Row Game");
-        setIconImage(new ImageIcon("./assets/chess.png").getImage());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //debug get working directory
+        String workingDir = System.getProperty("user.dir");
+
+        //check /assets/chess.png is existed in working directory
+        if(!new java.io.File(workingDir+"/assets/chess.png").exists()) {
+            throw new IOException("/assets/chess.png is not existed in working directory");
+        }else{
+            System.out.println("/assets/chess.png is existed in working directory");
+        }
+        //read chess.png from working directory
+        ImageIcon chessIcon = new ImageIcon(workingDir+"/assets/chess.png");
+        setIconImage(chessIcon.getImage());
     }
 }
--- src/test/GameFrameTest.java
+++ src/test/GameFrameTest.java
@@ -1,9 +1,10 @@
 import swing.GameFrame;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class GameFrameTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GameFrame frame = new GameFrame(6,50,10);
         EventQueue.invokeLater(()->{
             frame.setVisible(true);
