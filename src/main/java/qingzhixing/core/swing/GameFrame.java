7d6ca88092f00ae2c21aa9d97df928438efea9db
qingzhixing <1418390991@qq.com>
2022-08-27 16:13:46 +0800

    load icon

--- src/main/java/qingzhixing/core/swing/GameFrame.java
+++ src/main/java/qingzhixing/core/swing/GameFrame.java
@@ -1,8 +1,14 @@
 package qingzhixing.core.swing;
 
+import org.apache.log4j.Logger;
+
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.Objects;
 
 public class GameFrame extends JFrame {
+    private static final Logger logger = Logger.getLogger(GameFrame.class);
 
     /*
     根据棋盘大小，格子变长，内间距计算窗口大小
@@ -13,5 +19,16 @@ public class GameFrame extends JFrame {
         setResizable(false);
         setTitle("Five in a Row Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //load icon
+        String rootPath = Objects.requireNonNull(GameFrame.class.getResource("/")).getPath();
+        String iconPath = rootPath + "chess.png";
+        logger.debug("icon path: " + iconPath);
+        if (!new File(iconPath).exists()) {
+            logger.error("Icon file does not exists");
+        } else {
+            Image icon = new ImageIcon(iconPath).getImage();
+            setIconImage(icon);
+        }
     }
 }
